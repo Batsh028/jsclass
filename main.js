7ff7d09a80ab5d44efde5b1a01e3614e5cfd9f54
Batsh028 <54983221+Batsh028@users.noreply.github.com>
2019-10-03 08:49:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 class Person{
-    constructor(name,yolo){
+    constructor(name,age){
         this.name = name;
-        this.yolo = yolo;
+        this.age = age;
     }
     tellMyName(){
             return console.log(`I am ${this.name}`);
     }
 
     tellMyAge () {
-            return console.log(`I am ${this.yolo} years old`);
+            return console.log(`I am ${this.age} years old`);
     }
 }
 const john = new Person ("John",40);
 const mary = new Person ("Mary",35);
+
